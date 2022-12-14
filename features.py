f71585c288cb26e595024d9e39fc8a205c1510e0
ddl-joyce-zhao <joyce.zhao@dominodatalab.com>
2022-12-14 15:24:50 -0500

    Added features

---
+++ features.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+
+from feast import BigQuerySource, Entity, Feature, FeatureView, Field
+from feast.types import Float32, Int64, String
+
+driver_locations_source = BigQuerySource(
+    table="rh_prod.ride_hailing_co.drivers",
+    timestamp_field="event_timestamp",
+    created_timestamp_column="created_timestamp",
+)
+
+driver = Entity(
+    name="driver",
+    description="driver id",
+)
+
+driver_locations = FeatureView(
+    name="driver_locations",
+    entities=[driver],
+    ttl=timedelta(days=1),
+    schema=[
+        Field(name="lat", dtype=Float32),
+        Field(name="lon", dtype=String),
+        Field(name="driver", dtype=Int64),
+    ],
+    source=driver_locations_source,
+)
